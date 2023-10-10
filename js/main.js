5cfed9b3d7f8d26ad0c2987e541656e46934b542
thaiaren <thaiaren@gmail.com>
2023-10-10 23:54:06 +0700

    update

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
-console.log('hello');
+import axiosClient from './api/axiosClient';
+import postApi from './api/postApi';
+const data = {
+    id: 'lea319jollj7y1q8',
+    title: 'thai',
+};
+
+async function main() {
+    try {
+        const queryParams = {
+            _page: 1,
+            _limit: 5,
+        };
+        const reponse = await postApi.getById('lea319jollj7y1q8');
+        console.log(reponse);
+    } catch (error) {
+        console.log('error', error);
+    }
+}
+
+main();
